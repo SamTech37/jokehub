c9a6bf5ba29558371777489752db6c40b66fcd5a
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2021-09-05 01:11:50 +0800

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import NavBar from "./Components/NavBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </>
   );
 }
 
--- src/colors.js
+++
@@ -1,5 +0,0 @@
-export const color = {
-  primary: "",
-  secondary: "",
-  tertiary: "",
-};

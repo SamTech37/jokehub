a6e18efc2eb34a890e09bb9fdb13e89a13ccf29f
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2021-09-09 22:27:40 +0800

    ins styled comp

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
+const Nav = styled.div`
+  background: #ffff4d;
+`;
 export default function NavBar() {
   return (
-    <div>
-      <h1>wut</h1>
-    </div>
+    <Nav>
+      <h1>Joke Hub</h1>
+      <h2>profile</h2>
+      <h2>Post</h2>
+    </Nav>
   );
 }

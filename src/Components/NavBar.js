8fc083858ba2f28fb317331706e3e8fd1278a8ea
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2021-09-12 16:16:35 +0800

    polish navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,27 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Nav = styled.div`
   background: #fdfd66;
   height: 100px;
   display: flex;
   flex-direction: row;
-  h2 {
+  button {
     margin: 1em;
+    background: transparent;
+    font-size: 1.5em;
+    border-width: 0;
+    &:hover {
+      color: #0000005c;
+    }
   }
 `;
 export default function NavBar() {
   return (
     <Nav>
-      <h2>
-        <Link to="/">JokeHub</Link>
-      </h2>
-      <h2>
-        <Link to="/profile">Profile</Link>
-      </h2>
-      <h2>
-        <Link to="/post">Post</Link>
-      </h2>
+      <NavLink to="/">
+        <button>JokeHub</button>
+      </NavLink>
+      <Link to="/profile">
+        <button>Profile</button>
+      </Link>
+      <Link to="/post">
+        <button>Post</button>
+      </Link>
     </Nav>
   );
 }

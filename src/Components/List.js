130cd6b754434bfa57b66e1af376a9b6cae33a8b
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2021-10-18 01:03:59 +0800

    styling post body

--- src/Components/List.js
+++ src/Components/List.js
@@ -6,23 +6,22 @@ export default function List() {
     {
       title: "What do you call a magic owl?",
       content: "Hoodini!",
+      rate: 10,
+      poster: "someone",
       id: "0",
     },
     {
       title: "What's the heaviest stuff in my pants?",
       content: "Your mom",
+      rate: 7,
+      poster: "scankhunt69",
       id: "1",
     },
   ];
   return (
     <div>
       {initialData.map((joke) => {
-        return (
-          <>
-            <h2>{joke.title}</h2>
-            <h3>{joke.content}</h3>
-          </>
-        );
+        return <Post joke={joke}></Post>;
       })}
     </div>
   );
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,8 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
-const Body = styled.div``;
+const Body = styled.div`
+  height: 100%;
+  border: 2px solid black;
+  border-radius: 1.5em;
+  margin: 2px;
+  padding: 1em;
+`;
 
-export default function Post({ title, content, rate, poster }) {
-  return <div></div>;
+export default function Post({ joke }) {
+  return (
+    <Body>
+      <h1>{joke.title}</h1>
+      <h2>{joke.content}</h2>
+      <h3>{"rate: " + joke.rate}</h3>
+      <h3>{"poster: " + joke.poster}</h3>
+    </Body>
+  );
 }

78fc2c1e4c603b0bff37b3ed49c81e52dbd63dca
Sam <samkondori@gmail.com>
2021-11-06 15:06:29 +0800

    working on posting

--- src/Components/PostingPage.js
+++ src/Components/PostingPage.js
@@ -1,5 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
+const SendButton = styled.button`
+  background-color: orange;
+  color: white;
+  width: 200px;
+  height: 100px;
+`;
 export default function PostingPage() {
-  return <div></div>;
+  return (
+    <div>
+      <SendButton onClick={() => console.log("clicked send")}>Send</SendButton>
+    </div>
+  );
 }

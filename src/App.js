783eee1fb2079ed739bf7a35ab718b0360533b9a
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2021-11-13 22:11:48 +0800

    provide auth with google

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoginPage from "./Components/LoginPage";
 //routing
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //DB
-import { db } from "./firebaseConfig";
+import { db, auth } from "./firebaseConfig";
 import {
   collection,
   getDocs,
@@ -16,26 +16,58 @@ import {
   updateDoc,
   doc,
 } from "firebase/firestore";
+//auth
+import {
+  signInWithRedirect,
+  getRedirectResult,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 function App() {
+  //DB
   const [posts, setPosts] = useState([]);
   const postsColletionRef = collection(db, "posts");
-
   const postJoke = async (newTitle, newContent) => {
     //write data
     await addDoc(postsColletionRef, {
       title: newTitle,
       content: newContent,
-      author: "user",
+      author: user.displayName,
     });
   };
-
   const editJoke = async (id, editTitle, editContent) => {
     const jokeDoc = doc(db, "posts", id);
     const newFields = { title: editTitle, content: editContent };
     await updateDoc(jokeDoc, newFields);
   };
+  //Auth
+  const [user, setUser] = useState({});
+  const provider = new GoogleAuthProvider();
 
+  const userSignIn = async () => {
+    await signInWithRedirect(auth, provider);
+    await getRedirectResult(auth)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        //const credential = GoogleAuthProvider.credentialFromResult(result);
+        //const token = credential.accessToken;
+        setUser(result.user);
+      })
+      .catch((error) => {
+        const errorCode = error.Code;
+        console.log("error");
+      });
+  };
+
+  const userSignOut = async () => {
+    await signOut(auth);
+  };
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+  //main
   useEffect(() => {
     const getPosts = async () => {
       //read data
@@ -63,7 +95,7 @@ function App() {
           </Route>
 
           <Route path="/login">
-            <LoginPage />
+            <LoginPage signIn={userSignIn} signOut={userSignOut} user={user} />
           </Route>
           <Route path="/:postId" children={<Post />} />
 

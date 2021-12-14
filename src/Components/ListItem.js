2cf6477bf0f18799c1ca8544034307ef61aa8099
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2021-12-15 01:38:03 +0800

    working on avg rating jar css

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -61,6 +61,33 @@ const Body = styled.div`
     padding: 4vw;
   }
 `;
+const AvgRatingJar = styled.div`
+  display: flex;
+  align-self: center;
+  justify-self: center;
+  p {
+    color: #ffab01;
+  }
+  .circle {
+    border: solid blue 5px;
+    height: 5em;
+    width: 5em;
+    border-radius: 40%;
+    box-shadow: 0 0 0 5px #4973ff88;
+    animation: spin 5s linear infinite;
+    color: black;
+    text-align: center;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 export default function ListItem({
   content,
   rates,
@@ -81,33 +108,42 @@ export default function ListItem({
   if (signed) {
     return (
       <Body>
-        <h2>{content}</h2>
-        <p>{rates + "  Rates"}</p>
-        <p>{"Total Rating: " + totRating}</p>
+        <div>
+          <h2>{content}</h2>
 
-        {ratedUsers.includes(user?.uid) ? (
-          <button onClick={() => alert("You have rated this!")}>Rated</button>
-        ) : (
-          <div>
-            <input
-              onChange={(event) =>
-                setUserRate(parseInt(event.target.value / 10))
-              }
-              type={"range"}
-              className="slider"
-            />
-            <h1>{Number(userRate)}</h1>
-            <button onClick={handleClick}>Rate</button>
+          {ratedUsers.includes(user?.uid) ? (
+            <button onClick={() => alert("You have rated this!")}>Rated</button>
+          ) : (
+            <div>
+              <input
+                onChange={(event) =>
+                  setUserRate(parseInt(event.target.value / 10))
+                }
+                type={"range"}
+                className="slider"
+              />
+              <h1>{Number(userRate)}</h1>
+              <button onClick={handleClick}>Rate</button>
+            </div>
+          )}
+        </div>
+        {/*
+        <AvgRatingJar>
+          <div className="circle">
+            <p>{"Avg"}</p>
           </div>
-        )}
+        </AvgRatingJar>*/}
+        <p>{rates + "  Rates"}</p>
       </Body>
     );
   } else {
     return (
       <Body>
-        <h2>{content}</h2>
+        <div>
+          <h2>{content}</h2>
+        </div>
+
         <p>{rates + "  Rates"}</p>
-        <p>{"Total Rating: " + totRating}</p>
       </Body>
     );
   }

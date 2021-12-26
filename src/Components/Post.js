7b8e79d899d2c165b720a66ee3cd4b7b643c6604
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2021-12-26 16:59:18 +0800

    inside a post

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,46 +1,33 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Spacer from "./Spacer";
+import ListItem from "./ListItem";
 import { useParams } from "react-router-dom";
-const Body = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  min-height: 500px;
-  padding: 0px 10vw;
 
-  h2 {
-    white-space: pre-wrap; //line breaking, mutiple spaces , etc.
-    font-size: 1.5em;
-  }
-  button {
-    color: #ffab01;
-    font-size: 1em;
-    margin: 1em;
-    margin-top: 3em;
-    padding: 0.25em 1em;
-    border: 2px solid #ffab01;
-    border-radius: 3px;
-    &:hover {
-      opacity: 0.5;
-    }
-  }
-`;
-
-export default function Post({ inPost }) {
+export default function Post({ inPost, rateJoke, user, signed }) {
   //fetch the joke by its ID here
   let { postId } = useParams();
-  const [curJoke, setCurJoke] = useState({});
+  const [joke, setJoke] = useState();
   useEffect(async () => {
-    const joke = await inPost(postId);
-    setCurJoke(joke);
+    try {
+      setJoke(await inPost(postId));
+    } catch (error) {
+      console.log("e");
+    }
   }, []);
-  return (
-    <div>
-      <Spacer />
-      <Body>
-        <h2 style={{ fontWeight: 500 }}>{curJoke.content}</h2>
-      </Body>
-    </div>
-  );
+  if (joke) {
+    return (
+      <ListItem
+        postId={joke.id}
+        content={joke.content}
+        rates={joke.rates}
+        totRating={joke.totalRating}
+        ratedUsers={joke.ratedUsers}
+        user={user}
+        signed={signed}
+        rateJoke={rateJoke}
+      ></ListItem>
+    );
+  } else {
+    return <h1>Loading</h1>;
+  }
 }

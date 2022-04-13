e826b49bd938a35e461279c136e1ad5c264ce186
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-04-13 17:15:39 +0800

    add Navbar

---
+++ components/Joke.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function joke() {
+  return <div>joke</div>;
+}
---
+++ components/Posting.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function posting() {
+  return <div>posting</div>;
+}

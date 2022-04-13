1ffb99f6c0dfcda19361de13171258defc577740
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-04-13 17:30:43 +0800

    cleaned boilerplate, styling Joke

--- components/Joke.js
+++ components/Joke.js
@@ -1,5 +1,11 @@
 import React from "react";
+import styles from "../styles/Joke.module.css";
 
 export default function joke() {
-  return <div>joke</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.score}>7</div>
+      <div className={styles.body}>Joke</div>
+    </div>
+  );
 }

c640594044a94eb8e59f319e3c1489205d27f5dc
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-04-23 14:36:58 +0800

    add Icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,16 @@
 import React from "react";
 import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import icon from "../assets/icon.svg";
 export default function Navbar() {
-  return <div className={styles.body}>Navbar</div>;
+  return (
+    <div className={styles.body}>
+      <Link href="/">
+        <a>
+          <Image src={icon} height={100} />
+        </a>
+      </Link>
+    </div>
+  );
 }

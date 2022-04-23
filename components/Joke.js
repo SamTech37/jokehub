a162a1dc6a504df114006718df3a1135faec1ef3
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-04-23 17:26:37 +0800

    added blob to wrap avg rate

--- components/Joke.js
+++ components/Joke.js
@@ -1,11 +1,31 @@
 import React from "react";
 import styles from "../styles/Joke.module.css";
 
-export default function joke() {
+export default function joke({ joke, blobPattern }) {
   return (
     <div className={styles.main}>
-      <div className={styles.score}>7</div>
-      <div className={styles.body}>Joke</div>
+      <style jsx>{`
+        .blob {
+          display: flex;
+          height: 15vw;
+          width: 15vw;
+          margin: 0;
+          justify-content: center;
+          align-items: center;
+          background-image: url(${blobPattern.src});
+          background-repeat: no-repeat;
+          background-position: center;
+          font-size: 32px;
+          font-weight: 500;
+          transition: 300ms;
+          @media screen and (max-width: 450px) {
+            height: 20vh;
+            width: 60%;
+          }
+        }
+      `}</style>
+      <div className="blob">7</div>
+      <div className={styles.body}>{joke}</div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import styles from "../styles/Home.module.css";
 import Joke from "../components/Joke";
+import blob from "../assets/blob.svg";
+import blob1 from "../assets/blob1.svg";
+import blob2 from "../assets/blob2.svg";
+import blob3 from "../assets/blob3.svg";
+
 export default function Home() {
+  const jokes = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac semper eros, vel ultricies augue. Duis ultricies at dolor id commodo. Aliquam scelerisque, orci id malesuada ultricies, enim urna bibendum dolor, eget cursus sem enim vitae odio. Praesent at elit ex. Sed posuere, lectus mollis eleifend elementum, urna erat luctus orci, eu auctor erat libero nec metus. Ut facilisis purus in efficitur mollis. Donec eget pulvinar purus.",
+    ":欸地震時有杯牛奶被震倒了，你知道為什麼嗎？:我知道，因為乳糖不耐震:白癡因為地震很大",
+    "有一天張雨生去森林裡出來的時候帶著一個木頭有人問他怎麼來的他說與生俱來的",
+  ];
+  const blobs = [blob, blob1, blob2, blob3];
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <Joke />
-        <Joke />
-        <Joke />
+        {jokes.map((j) => {
+          return (
+            <Joke
+              joke={j}
+              key={jokes.indexOf(j)}
+              blobPattern={blobs[Math.floor(Math.random() * blobs.length)]}
+            />
+          );
+        })}
       </main>
     </div>
   );

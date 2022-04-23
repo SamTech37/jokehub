4e78c2d9e860614ddbe86b70f9159c62ed59c19a
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-04-23 15:20:11 +0800

    add dropdown menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,9 +8,30 @@ export default function Navbar() {
     <div className={styles.body}>
       <Link href="/">
         <a>
-          <Image src={icon} height={100} />
+          <Image src={icon} height={80} />
         </a>
       </Link>
+      <DropdownMenu />
+    </div>
+  );
+}
+
+function DropdownMenu() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className={styles.menuBtn}>
+      <a onClick={() => setOpen(!open)}>😅</a>
+      {open && (
+        <div className={styles.menu}>
+          <Link href="/">
+            <a>首頁</a>
+          </Link>
+          <Link href="/about">
+            <a>關於</a>
+          </Link>
+          <a>安裝</a>
+        </div>
+      )}
     </div>
   );
 }

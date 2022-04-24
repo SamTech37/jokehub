2162a652aac661ed80218106051ac69a3af25d42
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-04-24 14:25:16 +0800

    wording adjust

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,11 +26,11 @@ function DropdownMenu() {
           <Link href="/">
             <a>首頁</a>
           </Link>
+          <Link href="/me">
+            <a>個人</a>
+          </Link>
           <Link href="/about">
             <a>關於</a>
-          </Link>
-          <Link href="/me">
-            <a>帳戶</a>
           </Link>
           <a>安裝</a>
         </div>

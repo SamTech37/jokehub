100d92b807f82761a5221b61e7d5553d9cb47987
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-04-26 18:41:02 +0800

    add a new blob pattern

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import blob from "../assets/blob.svg";
 import blob1 from "../assets/blob1.svg";
 import blob2 from "../assets/blob2.svg";
 import blob3 from "../assets/blob3.svg";
+import blob4 from "../assets/blob4.svg";
 
 export default function Home() {
   const jokes = [
@@ -11,7 +12,7 @@ export default function Home() {
     ":欸地震時有杯牛奶被震倒了，你知道為什麼嗎？:我知道，因為乳糖不耐震:白癡因為地震很大",
     "有一天張雨生去森林裡,出來的時候帶著一個木頭,有人問他木頭怎麼來的,他說與生俱來的",
   ];
-  const blobs = [blob, blob1, blob2, blob3];
+  const blobs = [blob, blob1, blob2, blob3, blob4];
   return (
     <div className={styles.container}>
       <main className={styles.main}>

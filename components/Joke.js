a1d6caaf581594a174a91b71b4f667f20ed5a983
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-05-28 03:22:48 +0800

    grid responsive design

--- components/Joke.js
+++ components/Joke.js
@@ -19,29 +19,12 @@ export default function Joke({
   return (
     <div className={isModal ? styles.modal : styles.main}>
       <style jsx>{`
-        .blob {
-          display: flex;
-          height: 15vw;
-          width: 15vw;
-          margin: 0;
-          justify-content: center;
-          align-items: center;
+        .pattern {
           background-image: url(${blobPattern.src});
-          background-repeat: no-repeat;
-          background-position: center;
-          font-size: 40px;
-          font-weight: 500;
-          font-family: "VT323";
-          transition: 300ms;
-        }
-        @media screen and (max-width: 450px) {
-          .blob {
-            color: red;
-          }
         }
       `}</style>
       <div className={styles.score}>
-        <div className="blob">
+        <div className={`${styles.blob} pattern`}>
           {Math.round((joke.totalRating / joke.rates) * 10) / 10 || "None"}
         </div>
         <Link href={`/u/${joke.posterUid}`} passHref>
@@ -63,65 +46,66 @@ export default function Joke({
           </a>
         )}
       </div>
-      <Range
-        values={score}
-        step={0.1}
-        min={0}
-        max={10}
-        onChange={(values) => setScore(values)}
-        renderTrack={({ props, children }) => (
-          <div
-            onMouseDown={props.onMouseDown}
-            onTouchStart={props.onTouchStart}
-            style={{
-              ...props.style,
-              height: "36px",
-              display: "flex",
-              width: "90%",
-              gridArea: "slide",
-            }}
-          >
+      <div className={styles.slide}>
+        <Range
+          values={score}
+          step={0.1}
+          min={0}
+          max={10}
+          onChange={(values) => setScore(values)}
+          renderTrack={({ props, children }) => (
             <div
-              ref={props.ref}
+              onMouseDown={props.onMouseDown}
+              onTouchStart={props.onTouchStart}
               style={{
-                height: "7px",
-                width: "100%",
-                borderRadius: "4px",
-                background: getTrackBackground({
-                  values: score,
-                  colors: ["#FFAB10", "#ccc"],
-                  min: 0,
-                  max: 10,
-                }),
-                alignSelf: "center",
+                ...props.style,
+                height: "36px",
+                display: "flex",
+                width: "90%",
               }}
             >
-              {children}
+              <div
+                ref={props.ref}
+                style={{
+                  height: "7px",
+                  width: "100%",
+                  borderRadius: "4px",
+                  background: getTrackBackground({
+                    values: score,
+                    colors: ["#FFAB10", "#ccc"],
+                    min: 0,
+                    max: 10,
+                  }),
+                  alignSelf: "center",
+                }}
+              >
+                {children}
+              </div>
             </div>
-          </div>
-        )}
-        renderThumb={({ props }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: "40px",
-              width: "40px",
-              borderRadius: "4px",
-              backgroundColor: "#FFF",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              boxShadow: "0px 2px 6px #AAA",
-              fontWeight: "500",
-              fontSize: "30px",
-              fontFamily: "VT323",
-            }}
-          >
-            {score[0].toFixed(0)}
-          </div>
-        )}
-      />
+          )}
+          renderThumb={({ props }) => (
+            <div
+              {...props}
+              style={{
+                ...props.style,
+                height: "40px",
+                width: "40px",
+                borderRadius: "4px",
+                backgroundColor: "#FFF",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                boxShadow: "0px 2px 6px #AAA",
+                fontWeight: "500",
+                fontSize: "30px",
+                fontFamily: "VT323",
+              }}
+            >
+              {score[0].toFixed(0)}
+            </div>
+          )}
+        />
+      </div>
       <p className={styles.tag}>{"#" + joke.keyword}</p>
 
       <div className={styles.share}>

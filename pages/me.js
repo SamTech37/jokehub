9703208d163a2d4a39a6aed84ebed17a7cf09883
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-06-01 17:17:50 +0800

    styling profile page

--- pages/me.js
+++ pages/me.js
@@ -14,17 +14,20 @@ export default function me({ user }) {
   return (
     <div className={styles.body}>
       {user ? (
-        <div>
-          <button onClick={userSignOut}>
+        <div className={styles.main}>
+          <button onClick={userSignOut} className={styles.signout}>
             <BiLogOut />
           </button>
           <img
             src={`https://avatars.dicebear.com/api/croodles-neutral/${user?.uid}.svg`}
-            alt="poster"
-            width={100}
-            height={100}
+            alt="your avatar"
+            className={styles.doodle}
           />
-          <h1> {user?.displayName}</h1>
+          <div className={styles.profile}>
+            <h1>{"NickName"}</h1>
+            <p>you haven't set you nickname</p>
+            <h2>{"Bio:"}</h2>
+          </div>
         </div>
       ) : (
         <>

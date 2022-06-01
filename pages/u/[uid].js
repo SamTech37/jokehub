dc4a74c592f95102f5f9e05830ad7265cd0ec577
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-06-01 17:20:27 +0800

    styling userpage

--- pages/u/[uid].js
+++ pages/u/[uid].js
@@ -1,16 +1,22 @@
+import styles from "../../styles/Me.module.css";
 import React from "react";
 import { useRouter } from "next/router";
 export default function User() {
   const router = useRouter();
   const { uid } = router.query;
   return (
-    <div>
-      <img
-        src={`https://avatars.dicebear.com/api/croodles-neutral/${uid}.svg`}
-        alt="poster"
-        width={80}
-        height={80}
-      />
+    <div className={styles.body}>
+      <div className={styles.main}>
+        <img
+          src={`https://avatars.dicebear.com/api/croodles-neutral/${uid}.svg`}
+          alt="your avatar"
+          className={styles.doodle}
+        />
+        <div className={styles.profile}>
+          <h1>{"NickName"}</h1>
+          <h2>{"Bio:"}</h2>
+        </div>
+      </div>
     </div>
   );
 }

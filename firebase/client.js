375256f86a7be2b0aa1f2bc3139f4ffffbe7be54
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-06-04 07:10:53 +0800

    update dependencies

--- firebase/client.js
+++ firebase/client.js
@@ -13,6 +13,9 @@ import {
   serverTimestamp,
   doc,
   getDoc,
+  updateDoc,
+  increment,
+  arrayUnion,
 } from "firebase/firestore/lite";
 import {
   getAuth,
@@ -102,6 +105,15 @@ export const postJoke = async (content, keyword, uid) => {
     ratedUsers: [],
   });
   return newDoc.id;
+};
+
+const rateJoke = async (pid, uid, userRate) => {
+  if (userRate >= 0 && userRate <= 10)
+    await updateDoc(doc(db, "posts", pid), {
+      ratedUsers: arrayUnion(uid),
+      rates: increment(1),
+      totalRating: increment(userRate),
+    });
 };
 
 //auth

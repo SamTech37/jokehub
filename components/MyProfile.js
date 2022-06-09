cdebcd4594b6dfe59d54c2424c6a4a122d3cfd86
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-06-10 00:43:51 +0800

    styled /me

--- components/MyProfile.js
+++ components/MyProfile.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
 import styles from "../styles/Me.module.css";
 import { BiLogOut } from "react-icons/bi";
-import { userSignOut } from "../firebase/client";
+import { BsCheckLg } from "react-icons/bs";
+
+import { userSignOut, changeNickname, changeBio } from "../firebase/client";
 export default function MyProfile({ user }) {
-  const [profile, setProfile] = useState({
-    nickname: "暱稱",
-    bio: "自介",
-  });
-  const [newName, setNewName] = useState("");
-  const [newBio, setNewBio] = useState("");
-  const [edit, setEdit] = useState(false);
+  const [nickname, setNickname] = useState("暱稱");
+  const [bio, setBio] = useState(`按一下開始編輯`);
+
+  const handleUpdate = async () => {};
   return (
     <div className={styles.main}>
       <button onClick={userSignOut} className={styles.signout}>
@@ -20,14 +19,23 @@ export default function MyProfile({ user }) {
         alt="your avatar"
         className={styles.doodle}
       />
-      {edit ? (
-        <div>hi</div>
-      ) : (
-        <div className={styles.profile}>
-          <h2>{profile.nickname}</h2>
-          <h2>{profile.bio}</h2>
+      <div className={styles.profile}>
+        <input
+          className={styles.nickname}
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+          maxLength={10}
+        />
+        <textarea
+          className={styles.bio}
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+          maxLength={200}
+        />
+        <div className={styles.edit}>
+          <BsCheckLg className={styles.editbtn} />
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- firebase/client.js
+++ firebase/client.js
@@ -16,6 +16,7 @@ import {
   updateDoc,
   increment,
   arrayUnion,
+  setDoc,
 } from "firebase/firestore/lite";
 import {
   getAuth,
@@ -42,9 +43,23 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const postsRef = collection(db, "posts");
-
+const usersRef = collection(db, "users");
 //profile&users
-
+export const changeNickname = async (uid, newName) => {
+  await setDoc(doc(db, "users", uid), { nickname: newName }, { merge: true });
+};
+export const changeBio = async (uid, newBio) => {
+  await setDoc(doc(db, "users", uid), { bio: newBio }, { merge: true });
+};
+export const getProfile = async (uid) => {
+  const q = query(usersRef, limit(1), where(documentId(), "==", uid));
+  let snapshot = await getDocs(q);
+  if (snapshot.docs.length == 0) {
+    return "none";
+  } else {
+    return { ...snapshot.docs[0].data() }; //nickname,bio,link?
+  }
+};
 //jokes
 function makeid(length) {
   var result = "";

83c79795c4bfc7e6e607683f26e34897d1e2e0ab
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-06-12 01:13:40 +0800

    using next-seo

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,36 @@ import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import { useState } from "react";
 import { useAuth } from "../firebase/client";
+import { DefaultSeo } from "next-seo";
+import { useRouter } from "next/router";
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState();
   useAuth(setUser);
-
+  const router = useRouter();
   return (
     <>
+      <DefaultSeo
+        title="JokeHub | 笑是良藥"
+        defaultTitle="JokeHub"
+        description="A website where you can share jokes and rate others' joke. Laugh and enjoy. No censorship."
+        canonical={"https://jokehub.vercel.app" + router.asPath}
+        openGraph={{
+          type: "website",
+          url: "https://jokehub.vercel.app" + router.asPath,
+          title: "JokeHub | 笑是良藥",
+          description:
+            "A website where you can share jokes and rate others' joke. Laugh and enjoy. No censorship.",
+          site_name: "JokeHub",
+          images: [
+            {
+              url: "https://jokehub.vercel.app/og-image.png",
+              width: 1200,
+              height: 630,
+              alt: "JokeHub opengraph image",
+            },
+          ],
+        }}
+      />
       <Navbar user={user}></Navbar>
       <Component {...pageProps} user={user} />
     </>

993031bada9ae76c9db1741f4db4e6bfd4cf8781
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2022-06-14 23:59:32 +0800

    testing custom install

--- pages/install.js
+++ pages/install.js
@@ -1,14 +1,26 @@
-import React from "react";
-import PWAPrompt from "react-ios-pwa-prompt";
+import React, { useState, useEffect } from "react";
 
 export default function Install() {
+  const [event, setEvent] = useState();
+  function storeEvent(e) {
+    e.preventDefault();
+    setEvent(e);
+    console.log(`event was fired.`);
+  }
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", storeEvent);
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", storeEvent);
+      console.log(`event was cleaned.`);
+    };
+  }, []);
   return (
     <div>
-      <PWAPrompt />
       <h1>安裝 JokeHub App</h1>
       <h2>IOS</h2>
       <h2>Android</h2>
-      <button>安裝</button>
+      <button onClick={() => console.log(event)}>安裝</button>
     </div>
   );
 }
